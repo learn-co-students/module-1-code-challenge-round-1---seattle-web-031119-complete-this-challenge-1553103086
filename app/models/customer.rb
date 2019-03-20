0dc373e9a1bac6140f67cb82868580c060839ae2
github username <github email address>
2019-03-20 11:29:31 -0700

    Done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,46 @@
 class Customer
-  attr_accessor :first_name, :last_name
+  attr_reader :first_name, :last_name
+  @@all = []
 
   def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name  = last_name
+    @first_name = first_name.capitalize
+    @last_name  = last_name.capitalize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(search_name)
+    self.all.find {|customer| customer.full_name.downcase == search_name.downcase}
+  end
+
+  def self.find_all_by_first_name(search_first_name)
+    self.all.select {|customer| customer.first_name.downcase == search_first_name.downcase}
+  end
+
+  def self.all_names
+    self.all.collect {|customer| customer.full_name}
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def reviews
+    Review.all.select {|review| review.customer == self}
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(restaurant, self, content, rating)
+  end
+
+  def num_reviews
+    reviews.count
+  end
+
+  def restaurants
+    reviews.collect {|review| review.restaurant}.uniq
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,22 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+jon = Customer.new("jon", "isweird")
+jon2 = Customer.new("jon", "issuperweird")
+mera = Customer.new("mera", "person")
+joe = Customer.new("joe", "dude")
+
+gyro_stop = Restaurant.new("gyro stop")
+mai_thaiku = Restaurant.new("Mai Thaiku")
+
+r1 = Review.new(mai_thaiku, jon, "Tha bomb", 5)
+r2 = Review.new(mai_thaiku, mera, "actually the best", 5)
+r3 = Review.new(gyro_stop, joe, "meh", 3)
+
+jon.add_review(gyro_stop, "good greek fries", 4)
+jon2.add_review(mai_thaiku, "seriously go here", 5)
+mera.add_review(gyro_stop, "iz aight", 3)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
