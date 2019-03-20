df6b953ef05c9809aa5b58b77660e89b2955e0f2
quinncidences <qrc.inpa@gmail.com>
2019-03-20 11:34:26 -0700

    oh yeah.  first challenge complete

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,49 @@
 class Customer
-  attr_accessor :first_name, :last_name
+  attr_accessor :first_name, :last_name, :restaurant, :rating, :author, :review_count
+
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+    @review_count = 0
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_review(restaurant, rating, content)
+    Review.new(self, restaurant, rating, content)
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.find_by_name(name)
+    Customer.all.find {|customer| customer.full_name == name}
+  end
+
+  def self.find_all_by_first_name(name)
+    Customer.all.select {|customer| customer.first_name == name}
+  end
+
+  def self.all_names
+    Customer.all.map {|customer| customer.full_name}
+  end
+
+  def num_reviews
+    self.review_count
+  end
+
+  def all_reviews
+    Review.all.select {|review| review.author == self}
+  end
+
+  def restaurants
+    all_reviews.map {|review| review.restaurant}
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,45 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    Restaurant.all.find {|restaurant| restaurant.name == name}
+  end
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    reviews.map {|review| review.customer}
+  end
+
+  def average_star_rating
+    counter = 0
+    number_of_reviews = reviews.count
+    reviews.each {|review| counter += review.rating}
+    average_rating = counter / number_of_reviews
+  end
+
+  def longest_review
+    longest_content = nil
+    review_length = 0
+    reviews.each {|review| }
+      if review.content.split("").count > review_length
+        longest_content = review.content
+      end
+      longest_content
+    ##This might be complete, but I haven't tested
   end
 
 end
