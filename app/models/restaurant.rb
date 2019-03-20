aa6f0e15c04915aa1d5f79f3b20debbacc4710cc
github username <github email address>
2019-03-20 11:34:58 -0700

    Done except I didn't have enough time

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,55 @@
 class Restaurant
+
+  @@all = []
   attr_accessor :name
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|restaurant| restaurant.name == name}
+  end
+
+  def customers
+    my_reviews = Reviews.all.select {|review| review.restaurant == self}
+
+    my_reviews.map {|review| review.customer}.uniq
+  end
+
+  def reviews
+    my_reviews = Reviews.all.select {|review| review.restaurant == self}
+  end
+
+  def average_star_rating
+    my_reviews = Reviews.all.select {|review| review.restaurant == self}
+
+    ratings = my_review.map {|review| review.rating}
+
+    total =ratings.inject {|sum,n| sum + n}
+
+    return total/ratings.count
+
+  end
+
+  def longest_review
+      my_reviews = Reviews.all.select {|review| review.restaurant == self}
+
+      longest_one = ''
+
+      my_reviews.each do |review|
+        if review.content.length > longest_one.legnth
+          longest_one = review.content
+        else
+          nil
+        end
+      end
+      longest_one 
+  end
+
+  def self.all
+    @@all
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,74 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+puts "---------- object initialize ----------"
+puts
+jon = Customer.new("Jon","Langkammer")
+brit = Customer.new("Brit","Lutz")
+doug = Customer.new("Doug","Wort")
+sayed = Customer.new("Sayed","Raym")
+
+thai_kitchen = Restaurant.new("Thai Kitchen")
+indian_cuisine_gw = Restaurant.new("Indian Cuisine Greenwood")
+chipotle = Restaurant.new("Chipotle")
+fremont_brew = Restaurant.new("Fremont Brewing")
+olive_and_grape = Restaurant.new("Olive & Grape")
+mcdons = Restaurant.new("McDonalds")
+
+r1 = Review.new(jon,thai_kitchen,4,"Their curry is very good")
+r2 = Review.new(jon,mcdons,1,"Has been poisoning our country for decades")
+r3 = Review.new(brit,fremont_brew,4,"Sister IPA is great!")
+r4 = Review.new(sayed,fremont_brew,3,"I don't drink")
+r5 = Review.new(doug,chipotle,4,"Soooo good")
+r5 = Review.new(doug,olive_and_grape,5,"Soooo soo good")
+r5 = Review.new(jon,olive_and_grape,5,"very tastey stuff")
+
+
+puts "---------- Class Methods & Properties ----------"
+puts
+puts "Customer.all"
+puts Customer.all
+puts
+puts "Customer.find_by_name(Jon Langkammer)"
+puts Customer.find_by_name("Jon Langkammer")
+puts
+puts "Customer.find_all_by_first_name(doug)"
+puts Customer.find_all_by_first_name("doug")
+puts
+puts "Customer.all_names"
+puts Customer.all_names
+puts
+puts "Restaurant.all"
+puts Restaurant.all
+puts
+puts "Restaurant.find_by_name(chipotle)"
+puts Restaurant.find_by_name("chipotle")
+
+puts "---------- Association Methods ----------"
+
+
+puts
+puts "jon.num_reviews"
+puts jon.num_reviews
+puts
+puts "jon.restaurants"
+puts jon.restaurants
+puts
+puts olive_and_grape.customers
+
+puts "Ran out of time! :("
+
+
+
+
+
+
+
+
+
+
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
