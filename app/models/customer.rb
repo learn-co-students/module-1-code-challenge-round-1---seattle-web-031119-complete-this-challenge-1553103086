6c89cfbe4eb1f5e9589bfb84fda091d5189d0f7a
Rylan Bauermeister <apple@MacBook-Pro.local>
2019-03-20 11:29:27 -0700

    Code challenge 1 woo

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,49 @@
+
 class Customer
   attr_accessor :first_name, :last_name
+
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, content, rating)
+  end
+
+  def reviews
+    Review.all.select{|review| review.customer == self}
+  end
+
+  def num_reviews
+    self.reviews.count
+  end
+
+  def restaurants
+    self.reviews.map{|review| review.restaurant}.uniq
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all_names
+    @@all.map{|customer| customer.full_name}
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|customer| customer.full_name == name}
+  end
+
+  def self.find_by_first_name(fname)
+    @@all.select{|customer| customer.first_name == fname}
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,49 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def reviews
+    Review.all.select{|review| review.restaurant == self}
+  end
+
+  def num_reviews
+    self.reviews.count
+  end
+
+  def customers
+    self.reviews.map{|review| review.customer}.uniq
+  end
+
+  def ratings
+    self.reviews.map{|review| review.rating}
+  end
+
+  def sum_ratings
+    self.ratings.reduce(0) {|acc, rating| acc += rating}
+  end
+
+  def average_star_rating
+    has_reviews? ? self.sum_ratings / self.num_reviews : "unrated"
+  end
+
+  def has_reviews?
+    reviews.count > 0
+  end
+
+  def longest_review
+    has_reviews? ? self.reviews.max_by{|review| review.content.length}.content : ""
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|restaurant| restaurant.name == name}
+  end
+
+  def self.all
+    @@all
+  end
 end
