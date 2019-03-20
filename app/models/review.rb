52ac73894d051e6f78b8dd5de84041509c061d7b
jyang81 <joe.yang.design@gmail.com>
2019-03-20 11:31:07 -0700

    Doney done done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,30 @@
 class Review
-  
-end
 
+ attr_accessor :restaurant, :rating, :content
+ attr_reader :customer
+
+ @@all = []
+
+ def initialize(customer, restaurant, rating, content)
+   @customer = customer
+   @restaurant = restaurant
+   @rating = rating.to_i
+   ######couldn't figure it out in time
+   #  if @rating.between?(1,5)
+   #    @rating = rating
+   #  else
+   #    nil
+   #  end
+   # @rating
+   #######
+   @content = content
+   @@all << self
+ end
+
+ def self.all
+   @@all
+ end
+
+
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,30 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+abe = Customer.new("Abe","Lee")
+joe = Customer.new("Joe","Yang")
+joe2 = Customer.new("Joe","Kim")
+tim = Customer.new("Tim","Smith")
+puts "--------------Customers------------------"
+p Customer.all
+puts
+canlis = Restaurant.new("Canlis")
+mcd = Restaurant.new("McDonalds")
+applebees = Restaurant.new("Applebees")
+puts "--------------Restaurants------------------"
+p Restaurant.all
+puts
+r1 = Review.new(abe, canlis, 5, "Awesome")
+r2 = Review.new(joe, mcd, 1, "I'm not lovin it")
+r3 = Review.new(joe, applebees, 2, "Meh")
+r3a = Review.new(joe, applebees, 3, "Better")
+r4 = Review.new(joe2, applebees, 4, "So good")
+r5 = Review.new(tim, applebees, 5, "My favorite")
+r5 = Review.new(tim, mcd, 1, "Ewwww")
+puts "--------------Reviews------------------"
+p Review.all
+puts
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
