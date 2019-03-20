34ae9297506abf01284b166f34dbb6e1e12c9fa9
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-03-20 11:36:33 -0700

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,51 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    all.find {|resto|
+      resto.name == name
+    }
+  end
+
+  def customers
+    self.reviews.collect {|review|
+      review.customer
+    }.uniq
+  end
+
+  def reviews
+    Review.all.select {|review|
+      review.restaurant == self
+    }
+  end
+
+  def ratings
+    self.reviews.collect {|review|
+      review.rating
+    }
+  end
+
+  def average_star_rating
+    self.ratings.inject(0) {|sum, rating|
+      sum + rating
+    }.to_f / self.ratings.size
+  end
+
+  def longest_review
+    self.reviews.max_by {|review|
+      review.content.length
+    }.content
   end
 
 end
